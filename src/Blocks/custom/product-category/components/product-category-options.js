e3c4ab0528ceb81d14a47d19c4aa34cf165c294c
zero4281 <zero4281@gmail.com>
2025-06-25 17:28:11 -0500

    Product Category Block

---
+++ src/Blocks/custom/product-category/components/product-category-options.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import apiFetch from '@wordpress/api-fetch';
+import { TextControl } from '@wordpress/components';
+import {
+	icons,
+	IconLabel,
+	IconToggle,
+	Section,
+	checkAttr,
+	getAttrKey,
+	Select,
+} from '@eightshift/frontend-libs/scripts';
+import manifest from './../manifest.json';
+
+export const ProductCategoryOptions = ({ attributes, setAttributes }) => {
+	
+	const productCategoryName = checkAttr('productCategoryName', attributes, manifest);
+	const productCategoryBorder = checkAttr('productCategoryBorder', attributes, manifest);
+	const productCategoryBorderThick = checkAttr('productCategoryBorderThick', attributes, manifest);
+	const productCategoryDisplayGrandchildren = checkAttr('productCategoryDisplayGrandchildren', attributes, manifest);
+	const productCategoryFormat = checkAttr('productCategoryFormat', attributes, manifest);
+	
+	return (
+		<>
+			<Section icon={icons.tools} label={__('Category Name', 'delta9-digital-blocks-plugin')} >
+				<Select
+		        	label={__('Category Name', 'delta9-digital-blocks-plugin')}
+		        	value={productCategoryName}
+		        	options={manifest.allowed.categories}
+					onChange={(value) => setAttributes({ [getAttrKey('productCategoryName', attributes, manifest)]: value })}
+		        />
+			</Section>
+			
+			<IconToggle
+		        icon={icons.width}
+		        label={__('Show Border', 'delta9-digital-blocks-plugin')}
+		        checked={productCategoryBorder}
+		        onChange={(value) => setAttributes({ [getAttrKey('productCategoryBorder', attributes, manifest)]: value })}
+	        />
+	        
+	        {productCategoryBorder && (
+		        <IconToggle
+			        icon={icons.width}
+			        label={__('Show Thick Border', 'delta9-digital-blocks-plugin')}
+			        checked={productCategoryBorderThick}
+			        onChange={(value) => setAttributes({ [getAttrKey('productCategoryBorderThick', attributes, manifest)]: value })}
+		        />
+	        )}
+	        
+	        <IconToggle
+		        icon={icons.width}
+		        label={__('Display Grandchildren', 'delta9-digital-blocks-plugin')}
+		        checked={productCategoryDisplayGrandchildren}
+		        onChange={(value) => setAttributes({ [getAttrKey('productCategoryDisplayGrandchildren', attributes, manifest)]: value })}
+	        />
+	        
+			<Section icon={icons.tools} label={__('Category Display Format', 'delta9-digital-blocks-plugin')} >
+				<Select
+		        	label={__('Category Display Format', 'delta9-digital-blocks-plugin')}
+		        	value={productCategoryFormat}
+		        	options={manifest.allowed.displayFormat}
+					onChange={(value) => setAttributes({ [getAttrKey('productCategoryFormat', attributes, manifest)]: value })}
+		        />
+			</Section>
+		</>
+	);
+	
+	/*
+	let parentCategories;
+	let wpNonce = 0;
+	
+	const params = {
+		parent: 0
+	};
+	
+	//TODO: Set up a REST API endpoint in WordPress to return a Nonce if the user is logged in or use the WooCommerce API keys.
+	//      Code examples here: https://wordpress.stackexchange.com/questions/359056/woocommerce-rest-api-ajax-auth-401-response
+	
+	jQuery.ajax({
+		type: 'GET',
+		url: '/wp-json/wc/v3/products/categories?_wpnonce=' + wpNonce,
+		data: params,
+		dataType: 'json'
+	}).success(function (response) {
+		console.log(response);
+	}).always(function (response) {
+		console.log(response);
+	});
+	*/
+
+};
---
+++ src/Blocks/custom/product-category/product-category.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Template for the Product Category Block.
+ *
+ * @package Delta9DigitalBlocksPlugin
+ */
+
+use Delta9DigitalBlocksPluginVendor\EightshiftLibs\Helpers\Helpers;
+
+$manifest = Helpers::getManifestByDir(__DIR__);
+
+$unique = Helpers::getUnique();
+
+$componentClass = $manifest['componentClass'] ?? '';
+$additionalClass = $attributes['additionalClass'] ?? '';
+$blockClass = $attributes['blockClass'] ?? '';
+$selectorClass = $attributes['selectorClass'] ?? $componentClass;
+
+$productCategoryName = (Helpers::checkAttr('productCategoryName', $attributes, $manifest))['value'];
+$productCategoryBorder = Helpers::checkAttr('productCategoryBorder', $attributes, $manifest);
+$productCategoryBorderThick = Helpers::checkAttr('productCategoryBorderThick', $attributes, $manifest);
+$productCategoryFormat = (Helpers::checkAttr('productCategoryFormat', $attributes, $manifest))['value'];
+$productCategoryDisplayGrandchildren = Helpers::checkAttr('productCategoryDisplayGrandchildren', $attributes, $manifest);
+
+if($productCategoryBorder) {
+	$blockClass .= " product-category-format-border";
+}
+
+if($productCategoryBorderThick) {
+	$blockClass .= " product-category-format-border-thick";
+}
+
+if($productCategoryFormat == 'Stacked') {
+	$blockClass .= " product-category-format-stacked";
+} elseif($productCategoryFormat == 'Reverse Order') {
+	$blockClass .= " product-category-format-reverse-order";
+}
+
+global $product;
+
+$categoriesArray = array();
+$productParentCats = array();
+$productChildCats = array();
+$productCategories = get_the_terms( $product->ID, 'product_cat' );
+
+foreach($productCategories as $prodCat) {
+	if($prodCat->parent == 0) {
+		$productParentCats[] = $prodCat->term_id;
+	} else {
+		$productChildCats[] = $prodCat->term_id;
+	}
+}
+
+$args = array(
+    'taxonomy' => 'product_cat',
+    'hide_empty' => false,
+    'parent'   => 0
+);
+$product_cat = get_terms( $args );
+
+foreach ($product_cat as $parent_product_cat) {
+	if(in_array($parent_product_cat->term_id, $productParentCats) && $parent_product_cat->name == $productCategoryName) {
+		$categoriesArray[$parent_product_cat->name]['object'] = $parent_product_cat;
+		
+		$child_args = array(
+		            'taxonomy' => 'product_cat',
+		            'hide_empty' => false,
+		            'parent'   => $parent_product_cat->term_id
+		        );
+		$child_product_cats = get_terms( $child_args );
+		
+		// Subcategories
+		foreach ($child_product_cats as $child_product_cat) {
+			if(in_array($child_product_cat->term_id, $productChildCats)) {
+				$categoriesArray[$parent_product_cat->name]['children'][$child_product_cat->term_id]['object'] = $child_product_cat;
+				
+				$subcat_child_args = array(
+		            'taxonomy' => 'product_cat',
+		            'hide_empty' => false,
+		            'parent'   => $child_product_cat->term_id
+		        );
+			    $subcat_product_cats = get_terms( $subcat_child_args );
+				
+				foreach($subcat_product_cats as $subcat) {
+					if($subcat->parent == $child_product_cat->term_id && $subcat->count != 0) {
+						$categoriesArray[$parent_product_cat->name]['children'][$child_product_cat->term_id]['children'][] = $subcat;
+					}
+				}
+			}
+		}
+	}
+}
+
+echo '<div class="' . $blockClass . ' product-categories-container">';
+
+if(!empty($categoriesArray)) {
+	foreach($categoriesArray as $parentCategory) {
+		if(count($parentCategory['children']) > 0) {
+			foreach($parentCategory['children'] as $catChild) {
+				$catChildObj = $catChild['object'];
+				
+				echo '<div class="product-category-container">';
+					echo '<span class="product-category-name">' . $catChildObj->name . '</span>';
+					
+					if(isset($catChild['children']) && $productCategoryDisplayGrandchildren) {
+						foreach($catChild['children'] as $subCat) {
+							echo '<span class="product-category-subcategory">' . $subCat->name . '</span>';
+						}
+					}
+					
+				echo "</div>";
+			}
+		}
+	}
+}
+echo "</div>";
+
+?>
